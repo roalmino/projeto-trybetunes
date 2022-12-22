ab2622c9881e308a0c72d833fde581db4f35acdb
Rodrigo Almino <rdg.almino@gmail.com>
2022-12-22 13:26:51 -0300

    Requesito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,58 @@
 import React from "react";
+import { createUser } from "../services/userAPI";
+import Loading from "./Loading";
 
 class Login extends React.Component {
+  state = {
+    name: "",
+    loading: false,
+  };
+
+  montagem = () => {
+    const { name } = this.state;
+    const { history } = this.props;
+    this.setState({ loading: true }, async () => {
+      await createUser({ name });
+      history.push("/search");
+    });
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      name: value,
+    });
+  };
+
   render() {
-    return <div data-testid="page-login">Login</div>;
+    const { loading, name } = this.state;
+    const isDisable = name.length > 2;
+    return loading ? (
+      <Loading />
+    ) : (
+      <div data-testid="page-login">
+        Login
+        <form>
+          <label>
+            Nome:
+            <input
+              type="text"
+              name="name"
+              data-testid="login-name-input"
+              onChange={this.handleChange}
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={!isDisable}
+            onClick={() => this.montagem()}
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
